02db1a252307fb67a06342e365e7ffa1b88483bf
Phillip Millwee <philmillwee2@gmail.com>
2016-08-07 19:09:33 -0400

    Added time tracking

--- server.js
+++ server.js
@@ -3,22 +3,33 @@
 const listener = require("./src/lib/listener");
 
 let queue = [];
+let startTime, stopTime;
 
 listener.start(function(err) {
   if (err) {
     console.log(err);
   } else {
+    console.time("time");
     console.log("[server] UDP services started successfully");
   }
 });
 
 listener.setEvent("message", function(clientMsg) {
   let timestamp = (new Date()).toISOString();
-  queue.push({
-    timestamp: timestamp,
-    buffer: clientMsg
-  });
-  console.log(queue.length, timestamp, clientMsg);
+  if(queue.length < 1000) {
+    queue.push({
+      timestamp: timestamp,
+      buffer: clientMsg
+    });
+  } else {
+    listener.stop(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.timeEnd("time");
+        console.log(queue.length);
+        console.log("Server shutdown successfully.");
+      }
+    });
+  }
 });
-
-console.log("Server shutdown successfully.");
